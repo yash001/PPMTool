b79cfad5d96366f0a43225f4827d065f4ac35a44
yash <mehtayash001@gmail.com>
2020-04-14 19:29:45 +0530

    setup project object validation

--- ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package io.SpringBootReatProject.ppmtool.domain;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.util.Date;
 
@@ -11,13 +15,20 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = "Project name is required")
 	private String projectName;
+	@NotBlank(message = "Project Identifier is required")
 	private String projectIdentifier;
+	@Size(min = 4,max = 5, message = "Please used 4 to 5 characters")
+	@NotBlank(message = "Project Description is required")
 	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date end_date;
-
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_At;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	
 	public Project() {
