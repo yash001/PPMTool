cec3609a7e9a91b41859bdcd7cac799f940f9c6b
yash <mehtayash001@gmail.com>
2020-04-15 18:41:41 +0530

    Find Project by Identifier - branch8

--- ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/repositories/ProjectRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import io.SpringBootReatProject.ppmtool.domain.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
-
-
+	
+	Project findByProjectIdentifier(String projectId);
 }
