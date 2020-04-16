3fa3fa230ce940028ba4f9ac737527de57a2415a
yash <mehtayash001@gmail.com>
2020-04-16 18:49:21 +0530

    first react component - branch12

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to PPMTool</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js is edited</code> and save to
-          reload.
-        </p>
+        <Dashboard />
       </div>
     );
   }
