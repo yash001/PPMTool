b9dc5547a4193589990dedc41f9815cb2a4c04d9
yash <mehtayash001@gmail.com>
2020-06-01 20:05:14 +0530

    Get Projects - redux only

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -14,7 +14,7 @@ class AddProject extends Component {
       description: "",
       start_date: "",
       end_date: "",
-      errors: {},
+      errors: {}
     };
 
     this.onChange = this.onChange.bind(this);
@@ -39,7 +39,7 @@ class AddProject extends Component {
       projectIdentifier: this.state.projectIdentifier,
       description: this.state.description,
       start_date: this.state.start_date,
-      end_date: this.state.end_date,
+      end_date: this.state.end_date
     };
     this.props.createProject(newProject, this.props.history);
   }
@@ -59,8 +59,8 @@ class AddProject extends Component {
                   <div className="form-group">
                     <input
                       type="text"
-                      className={classnames("form-control form-control-lg ", {
-                        "is-invalid": errors.projectName,
+                      className={classnames("form-control form-control-lg", {
+                        "is-invalid": errors.projectName
                       })}
                       placeholder="Project Name"
                       name="projectName"
@@ -76,15 +76,14 @@ class AddProject extends Component {
                   <div className="form-group">
                     <input
                       type="text"
-                      className={classnames("form-control form-control-lg ", {
-                        "is-invalid": errors.projectIdentifier,
+                      className={classnames("form-control form-control-lg", {
+                        "is-invalid": errors.projectIdentifier
                       })}
                       placeholder="Unique Project ID"
                       name="projectIdentifier"
                       value={this.state.projectIdentifier}
                       onChange={this.onChange}
                     />
-
                     {errors.projectIdentifier && (
                       <div className="invalid-feedback">
                         {errors.projectIdentifier}
@@ -93,8 +92,8 @@ class AddProject extends Component {
                   </div>
                   <div className="form-group">
                     <textarea
-                      className={classnames("form-control form-control-lg ", {
-                        "is-invalid": errors.description,
+                      className={classnames("form-control form-control-lg", {
+                        "is-invalid": errors.description
                       })}
                       placeholder="Project Description"
                       name="description"
@@ -144,11 +143,14 @@ class AddProject extends Component {
 
 AddProject.propTypes = {
   createProject: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) => ({
-  errors: state.errors,
+const mapStateToProps = state => ({
+  errors: state.errors
 });
 
-export default connect(mapStateToProps, { createProject })(AddProject);
+export default connect(
+  mapStateToProps,
+  { createProject }
+)(AddProject);
