f5a14b532e6ab5d45729164f7552d36666febe78
yash <mehtayash001@gmail.com>
2020-06-14 12:10:43 +0530

    Types and Reducers for Project Tasks - branch43

--- ppmtool-react-client/src/actions/types.js.bak
+++
@@ -1,2 +0,0 @@
-export const GET_ERRORS = "GET_ERRORS";
-export const GET_PROJECTS = "GET_PROJECTS";
--- ppmtool-react-client/src/reducers/projectReducer.js
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -2,29 +2,29 @@ import { GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "../actions/types";
 
 const initialState = {
   projects: [],
-  project: {},
+  project: {}
 };
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case GET_PROJECTS:
       return {
         ...state,
-        projects: action.payload,
+        projects: action.payload
       };
 
     case GET_PROJECT:
       return {
         ...state,
-        project: action.payload,
+        project: action.payload
       };
 
     case DELETE_PROJECT:
       return {
         ...state,
         projects: state.projects.filter(
-          (project) => project.projectIdentifier !== action.payload
-        ),
+          project => project.projectIdentifier !== action.payload
+        )
       };
     default:
       return state;
