c6ef6237a681662661f3357adf03e9946692d239
yash <mehtayash001@gmail.com>
2020-06-15 17:27:39 +0530

    AddProjectTask action ( )path) AddProjectTask form controlled part 2 -branch47

--- ppmtool-react-client/src/actions/backlogAction.js
+++ ppmtool-react-client/src/actions/backlogAction.js
@@ -4,5 +4,5 @@ export const addProjectTask = (backlog_id, project_task, history) => async (
   dispatch
 ) => {
   await axios.post(`/api/backlog/${backlog_id}`, project_task);
-  history.pushState(`/projectBoard/${backlog_id}`);
+  history.push(`/projectBoard/${backlog_id}`);
 };
--- ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -18,8 +18,31 @@ class AddProjectTask extends Component {
       projectIdentifier: id,
       erors: {},
     };
-  }
 
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  //on Change
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  //on Submit
+  onSubmit(e) {
+    e.preventDefault();
+    const newTask = {
+      summary: this.state.summary,
+      acceptanceCriteria: this.state.acceptanceCriteria,
+      status: this.state.status,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate,
+    };
+    console.log(newTask);
+    this.props.addProjectTask(
+      this.state.projectIdentifier,
+      newTask,
+      this.props.history
+    );
+  }
   render() {
     const { id } = this.props.match.params;
     return (
@@ -42,6 +65,7 @@ class AddProjectTask extends Component {
                     name="summary"
                     placeholder="Project Task summary"
                     value={this.state.summary}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -50,6 +74,7 @@ class AddProjectTask extends Component {
                     placeholder="Acceptance Criteria"
                     name="acceptanceCriteria"
                     value={this.state.acceptanceCriteria}
+                    onChange={this.onChange}
                   ></textarea>
                 </div>
                 <h6>Due Date</h6>
@@ -58,6 +83,8 @@ class AddProjectTask extends Component {
                     type="date"
                     className="form-control form-control-lg"
                     name="dueDate"
+                    value={this.state.dueDate}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -65,6 +92,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="priority"
                     value={this.state.priority}
+                    onChange={this.onChange}
                   >
                     <option value={0}>Select Priority</option>
                     <option value={1}>High</option>
@@ -78,6 +106,7 @@ class AddProjectTask extends Component {
                     className="form-control form-control-lg"
                     name="status"
                     value={this.state.status}
+                    onChange={this.onChange}
                   >
                     <option value="">Select Status</option>
                     <option value="TO_DO">TO DO</option>
