fab5b2c423e5df2a5e7750681f81c9ad1271bf4a
yash <mehtayash001@gmail.com>
2020-06-24 18:35:45 +0530

    Create User Object, Validation, Repository, Service - branch60

--- ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/security/JwtAuthenticationEntryPoint.java
+++ ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/security/JwtAuthenticationEntryPoint.java
@@ -1,32 +1,31 @@
 package io.SpringBootReatProject.ppmtool.security;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.google.gson.Gson;
+import io.SpringBootReatProject.ppmtool.exceptions.InvalidLoginResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import com.google.gson.Gson;
-
-import io.SpringBootReatProject.ppmtool.exceptions.InvalidLoginResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-	@Override
-	public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
-			AuthenticationException e) throws IOException, ServletException {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
 
-	
-	InvalidLoginResponse loginResponse = new InvalidLoginResponse();
-	String jsonLoginResponse = new Gson().toJson(loginResponse);
-	
-	httpServletResponse.setContentType("application/json"); 
-	httpServletResponse.setStatus(401);
-	httpServletResponse.getWriter().print(jsonLoginResponse);
- }
+        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(loginResponse);
+
+
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setStatus(401);
+        httpServletResponse.getWriter().print(jsonLoginResponse);
+
+
+    }
 }
