30d126f38f35b24e1e4eff5db13484f9766b50ee
yash <mehtayash001@gmail.com>
2020-06-28 10:54:14 +0530

    User registration part 1 - branch61

--- ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/services/UserService.java
+++ ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/services/UserService.java
@@ -1,8 +1,11 @@
 package io.SpringBootReatProject.ppmtool.services;
 
 
+import io.SpringBootReatProject.ppmtool.domain.User;
+import io.SpringBootReatProject.ppmtool.exceptions.UsernameAlreadyExistsException;
 import io.SpringBootReatProject.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +13,20 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser (User newUser){
+      newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+      //Username has to be unique (exception)
+
+        // Make sure that password and confirmPassword match
+        // We don't persist or show the confirmPassword
+      return userRepository.save(newUser);
+    }
 
 
 
