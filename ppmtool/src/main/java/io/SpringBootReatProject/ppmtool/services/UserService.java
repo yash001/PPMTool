76d321f86d2fd648e5eaf5df4dbf5978ec7355c4
yash <mehtayash001@gmail.com>
2020-06-28 21:15:49 +0530

    User registration part 2 - branch62

--- ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/services/UserService.java
+++ ppmtool/src/main/java/io/SpringBootReatProject/ppmtool/services/UserService.java
@@ -1,15 +1,43 @@
 package io.SpringBootReatProject.ppmtool.services;
 
 
+import io.SpringBootReatProject.ppmtool.domain.User;
+import io.SpringBootReatProject.ppmtool.exceptions.UsernameAlreadyExistsException;
 import io.SpringBootReatProject.ppmtool.repositories.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
+    
+    //@Autowired
+    //private PasswordEncoder  passwordEncoder;
+
+    public User saveUser (User newUser){
+
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            //Username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            // Make sure that password and confirmPassword match
+            // We don't persist or show the confirmPassword
+            return userRepository.save(newUser);
+
+        }catch (Exception e){
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
+
+    }
 
 
 
